18bb9e914c84171817b1cf1d18dd90e3dc3dc740
Erfan Nikouie <er.nikouie@gmail.com>
2024-01-26 01:51:15 +0330

    Update ConsoleCommand.cs

--- Assets/Scripts/ConsoleCommand.cs
+++ Assets/Scripts/ConsoleCommand.cs
@@ -11,30 +11,41 @@ public class ConsoleCommand : MonoBehaviour
     {
         Person person = new Person(name);
         PersonID pid = ftv.Controller.GetUniquePersonID();
-        ftv.Controller.AddPerson(person);
+        bool result = ftv.Controller.AddPerson(person);
         
-        Debug.Log($"Person with ID:{pid.Value} has been added. Hash:{person.Name}.");
+        if(result)
+            Debug.Log($"Person with ID:{pid.Value} has been added. Hash:{person.Name}.");
+        else
+            Debug.LogError("Person could not be added.");
     }
     
-    [Command("add-family", "Adds a family by its father and mother ids.")]
+    [Command("add-family", "Adds a family by its father and mother IDs.")]
     public void AddFamily(int fatherID, int motherID)
     {
         PersonID father = new PersonID(fatherID);
         PersonID mother = new PersonID(motherID);
         Family family = new Family(father, mother);
         FamilyID fid = ftv.Controller.GetUniqueFamilyID();
-        ftv.Controller.AddFamily(family);
+        bool result = ftv.Controller.AddFamily(family);
         
-        Debug.Log($"Family with ID:{fid.Value} has been added.");
+        if(result)
+            Debug.Log($"Family with ID:{fid.Value} has been added.");
+        else
+            Debug.LogError("Family could not be added.");
     }
     
-    [Command("add-child", "Adds a child (by its id) to a family (by its id).")]
+    [Command("add-child", "Adds a child (by its ID) to a family (by its ID).")]
     public void AddChild(int childID, int familyID)
     {
         PersonID child = new PersonID(childID);
         FamilyID family = new FamilyID(familyID);
-        ftv.Controller.AddChildToFamily(child, family);
+        bool result = ftv.Controller.AddChildToFamily(child, family);
         
-        Debug.Log($"Child with ID:{child.Value} has been added to the family with ID:{family.Value}.");
+        if(result)
+            Debug.Log($"Child with ID:{child.Value} has been added to the family with ID:{family.Value}.");
+        else
+            Debug.LogError("Child could not be added.");
     }
+    
+    
 }
