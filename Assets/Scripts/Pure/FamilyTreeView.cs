ba1a4dc562ff730d568fa6de4b50e3b29cb16513
Erfan Nikouie <er.nikouie@gmail.com>
2024-01-25 05:33:26 +0330

    Update FamilyTreeView.cs

--- Assets/Scripts/Pure/FamilyTreeView.cs
+++ Assets/Scripts/Pure/FamilyTreeView.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class FamilyTreeView : MonoBehaviour
 {
-    private FamilyTreeController controller;
+    private FamilyTreeController controller = null;
+
+    [SerializeField] private GameObject nodePrefab = null;
 
     private void Awake()
     {
@@ -11,6 +13,9 @@ public class FamilyTreeView : MonoBehaviour
         controller.RegisterAddPersonListener(Refresh);
         controller.RegisterAddFamilyListener(Refresh);
         controller.RegisterAddChildListener(Refresh);
+        
+        if(nodePrefab == null)
+            Debug.LogError("Node prefab is not set.");
     }
     
     private void Refresh()
